566e097096bbb9e330624aefb098caefc186b670
Matthias Ferstl <mferstl@live.de>
2024-10-10 18:32:57 +0200

    Added some simple lines of code

--- workingtitle.py
+++ workingtitle.py
@@ -1,1 +1,16 @@
-print("Open Hub, this is the police!")
+# I had to do something... so here we go - MF (Thought it would be great to sign comments with initials so we could blame each other)
+
+
+print("Open Hub, this is the police! Please state your name:")
+
+name = input ("Your name: ")
+
+print ("Thanks for being here with us, " + name + "!" )
+print ("What is your favourite toast?")
+
+magicKey = input ("We know you want it too: ")
+
+if (magicKey == "Buttertoast"):
+    print ("Great! So now, lets do some work!")
+else:  
+    print ("Disappointing!")
