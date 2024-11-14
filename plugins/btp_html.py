3016440ca00aa454989bb282dd23d20407585517
DoCL06383 <stefan.leippe@studium.fernuni-hagen.de>
2024-11-14 20:12:49 +0100

    FirstVersion_Tiff

--- plugins/btp_html.py
+++ plugins/btp_html.py
@@ -3,11 +3,8 @@ from Engine.plugin_Interface import plugin_Interface
 class Html(plugin_Interface):    
     def run(self, truecrypt, html_host):        
         offset = html_host.find(b'>')
-        if offset == -1:
-            print("No HTML found!")
-        else:
-            comment_start = (b'<!--')
-            comment_end = (b'//-->')    
-            polyglott = html_host[:offset+1] + comment_start + truecrypt[offset+5:] + comment_end + html_host[1+offset:]
-        
+        comment_start = (b'<!--')
+        comment_end = (b'//-->')    
+        polyglott = html_host[:offset+1] + comment_start + truecrypt[offset+5:] + comment_end + html_host[1+offset:]
+
         return polyglott
