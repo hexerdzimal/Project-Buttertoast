f9f829b2c2ac8c460a5ff60552cd85a1cb67051e
DoCL06383 <stefan.leippe@studium.fernuni-hagen.de>
2024-11-29 21:13:24 +0100

    Layer8 Problem in Essen

--- plugins/btp_zip.py
+++ plugins/btp_zip.py
@@ -1,17 +1,18 @@
 from Engine.plugin_Interface import plugin_Interface
 import struct
 
-class Bmp(plugin_Interface):
-    def run(self, truecrypt, bmp_host):
-        polyglot = bytearray(truecrypt) + bytearray(bmp_host)
-        bmp_header = bmp_host[:54]
-        bfOffBits_position = 10
-        new_bfOffBits = len(truecrypt)
-        modified_bmp_header = (
-                bmp_header[:bfOffBits_position] +
-                struct.pack('<I', new_bfOffBits) +
-                bmp_header[bfOffBits_position + 4:]
+class Zip(plugin_Interface):
+    def run(self, truecrypt, zip_host):
+        polyglot = bytearray(truecrypt) + bytearray(zip_host)
+        polyglot = bytes(polyglot)
+        eocd_signature = b'\x50\x4b\x05\x06'
+        eocd_index = polyglot.find(eocd_signature)
+        central_offset_position = eocd_index + 16
+        central_offset = struct.unpack("<I", polyglot[central_offset_position:central_offset_position + 4])[0]
+        new_central_offset = central_offset + len(truecrypt)
+        polyglot = (
+                polyglot[:central_offset_position]
+                + struct.pack("<I", new_central_offset)
+                + polyglot[central_offset_position + 4:]
         )
-        salt = modified_bmp_header + b'\x00' * (64 - len(modified_bmp_header))
-        polyglot = bytes(bytearray(salt) + bytearray(polyglot[64:]))
         return polyglot
