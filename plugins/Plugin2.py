457f5e49d5aac0ff3e47230c60cc3043c0ae65de
DoCL06383 <stefan.leippe@studium.fernuni-hagen.de>
2024-11-29 21:04:06 +0100

    Only working plugins bmp plugin added zip plugin added

--- plugins/Plugin2.py
+++
@@ -1,5 +0,0 @@
-from Engine.plugin_Interface import plugin_Interface
-
-class Plugin2(plugin_Interface):
-    def run(self):
-        print("Running Plugin 2")
--- plugins/btp_ico.py
+++ plugins/btp_ico.py
@@ -3,30 +3,23 @@ import struct
 
 class Ico(plugin_Interface):
     def run(self, truecrypt, ico_host):
-        # ICO-Header und Icon Directory Entry extrahieren
-        ico_header = ico_host[:6]  # Die ersten 6 Bytes der ICO-Datei
-        icon_directory_entry = ico_host[6:22]  # Nächste 16 Bytes für das Icon Directory Entry
+        atom_size = struct.pack('>I', len(truecrypt))
+        atom_type = b'skip'
+        major_brand = b'isom'
+        minor_version = b'\x00\x00\x00\x01'
+        compatible_brands = b'isomiso2avc1mp41'[:12]
+        skip_atom_salt = atom_size + atom_type + major_brand + minor_version + compatible_brands
+        skip_atom_salt += b'\x00' * (SALT_SIZE - len(skip_atom_salt))
+        polyglott = skip_atom_salt + truecrypt[65:] + mov_host
+        stco_index = polyglott.find(b'stco')
+        offset_count_index = stco_index + 8
+        offset_count = struct.unpack(">I", data[offset_count_index:offset_count_index + 4])[0]
+        offset_table_index = offset_count_index + 4
 
-        # Neuen Image Offset berechnen
-        new_image_offset = len(truecrypt) + 8  # Länge der TrueCrypt-Daten + Padding
-        modified_icon_directory_entry = (
-                icon_directory_entry[:12] +
-                struct.pack('<I', new_image_offset)  # Neuer Offset
-        )
+        for i in range(offset_count):
+            current_offset_index = offset_table_index + (i * 4)
+            current_offset = struct.unpack(">I", data[current_offset_index:current_offset_index + 4])[0]
+            adjusted_offset = current_offset + skip_atom_size
+            polyglott[current_offset_index:current_offset_index + 4] = adjusted_offset
 
-        # Padding definieren
-        padding = b'\x00' * 8
-
-        # Originale Bilddaten aus der ICO-Datei extrahieren
-        image_offset = struct.unpack('<I', icon_directory_entry[12:16])[0]
-        ico_image_data = ico_host[image_offset:]
-
-        # Polyglot-Datei zusammenstellen
-        polyglot = (
-                ico_header +
-                modified_icon_directory_entry +
-                truecrypt[22:] +
-                padding +
-                ico_image_data
-        )
-        return polyglot
+        return polyglott
